556b4d57e134147256d1592ad678da4bad83df8d
rayveneja <90801336+rayveneja@users.noreply.github.com>
2025-05-13 09:21:52 -0400

    updated backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,12 +2,22 @@ from fastapi import FastAPI
 from app.routes import auth, profile, events, interactions
 from app.database import engine
 from app.models import user
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 
 user.Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
+
 
 app.include_router(auth.router)
 app.include_router(profile.router)
@@ -17,3 +27,4 @@ app.include_router(interactions.router)
 @app.get("/")
 def read_root():
     return {"message": "Welcome to viberly API"}
+
